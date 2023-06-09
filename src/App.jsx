8cd7ee20d2688daa233fd72694278e029c46a8a3
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-09 06:51:21 +0530

    completed counter state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { useCounterStore } from "./zustand/store";
 
 export const App = () => {
-    return <div>App</div>;
+    const count = useCounterStore((state) => state.count);
+    const incrementCount = useCounterStore((state) => state.incrementCount);
+    const decrementCount = useCounterStore((state) => state.decrementCount);
+    const resetCount = useCounterStore((state) => state.resetCount);
+
+    return (
+        <section>
+            <h1>count: {count} </h1>
+            <div className="btn-box">
+                <button onClick={incrementCount}>increment</button>
+                <button onClick={decrementCount}>decrement</button>
+                <button onClick={resetCount}>reset</button>
+            </div>
+        </section>
+    );
 };
